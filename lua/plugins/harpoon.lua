636c854fe820ed59ff4e762916c2cd1506dce233
Will Deeley <wxllxm@Wills-MacBook-Pro.local>
2024-03-17 03:40:52 +0000

    Removed project navigation plugin

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -2,7 +2,7 @@ return {
   'ThePrimeagen/harpoon',
   config = function()
     require("telescope").load_extension('harpoon')
-    vim.keymap.set("n", "<Leader>h", ":Telescope harpoon marks<CR>", {})
+    vim.keymap.set("n", "<Leader>fh", ":Telescope harpoon marks<CR>", {})
     vim.keymap.set("n", "<Leader>ha", ":lua require('harpoon.mark').add_file()<CR>", {})
     vim.keymap.set("n", "<Leader>hn", ":lua require('harpoon.ui').nav_next()<CR>", {})
     vim.keymap.set("n", "<Leader>hp", ":lua require('harpoon.ui').nav_prev()<CR>", {})
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -13,4 +13,6 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
+vim.api.nvim_set_option("clipboard","unnamed")
+
 vim.wo.number = true
